3e276bb7c7d9decd17e8fa3a40e7241f4d2bfce3
maksimus <gorlovmaks23@gmail.com>
2020-11-05 12:17:38 +0600

    Обновлен роутер для исполнителей

--- app/artists.js
+++ app/artists.js
@@ -6,42 +6,39 @@ const config = require("../config");
 const Artist = require("../models/Artist");
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, config.uploadPath);
-  },
-  filename: (req, file, cb) => {
-    cb(null, nanoid() + path.extname(file.originalname));
-  }
+    destination: (req, file, cb) => {
+        cb(null, config.uploadPath);
+    },
+    filename: (req, file, cb) => {
+        cb(null, nanoid() + path.extname(file.originalname));
+    }
 });
 
 const upload = multer({storage});
 
-const createRouter = () => {
-  router.get('/', async (req, res) => {
+router.get('/', async (req, res) => {
     try {
-      const artists = await Artist.find();
-      res.send(artists);
+        const artists = await Artist.find();
+        res.send(artists);
     } catch {
-      res.sendStatus(500);
+        res.sendStatus(500);
     }
-  });
+});
 
-  router.post("/", upload.single("image"), async (req, res) => {
+router.post("/", upload.single("image"), async (req, res) => {
     const artistData = req.body;
 
     if (req.file) {
-      artistData.image = req.file.filename;
+        artistData.image = req.file.filename;
     }
 
     const artist = new Artist(artistData);
-    try{
-      await artist.save();
-      res.send(artist);
+    try {
+        await artist.save();
+        res.send(artist);
     } catch (e) {
-      res.status(400).send(e);
+        res.status(400).send(e);
     }
-  });
-  return router;
-};
+});
 
-module.exports = createRouter;
+module.exports = router;
