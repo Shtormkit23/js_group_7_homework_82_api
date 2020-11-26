b69d61172bb8f58303a34cf3cd298413e335bbee
maksimus <gorlovmaks23@gmail.com>
2020-11-26 19:01:31 +0600

    HW#89

--- app/artists.js
+++ app/artists.js
@@ -4,6 +4,9 @@ const path = require("path");
 const {nanoid} = require("nanoid");
 const config = require("../config");
 const Artist = require("../models/Artist");
+const Album = require("../models/Album");
+const auth = require("../middleware/auth");
+const permit = require("../middleware/permit");
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -18,7 +21,8 @@ const upload = multer({storage});
 
 router.get('/', async (req, res) => {
     try {
-        const artists = await Artist.find();
+        let published = {published: true}
+        const artists = await Artist.find(published);
         res.send(artists);
     } catch {
         res.sendStatus(500);
@@ -38,6 +42,30 @@ router.post("/", upload.single("image"), async (req, res) => {
         res.send(artist);
     } catch (e) {
         res.status(400).send(e);
+    }
+});
+
+router.put("/:id", [auth, permit('admin')], async (req, res) => {
+    try {
+        const artist = await Artist.findById(req.params.id);
+        artist.published = !artist.published
+        await artist.save();
+        return res.send({ message: `${req.params.id} published` });
+    } catch (e) {
+        return res.status(422).send(e);
+    }
+});
+
+router.delete("/:id", [auth, permit('admin')], async (req, res) => {
+    try {
+        const albums = await Album.find({artist: req.params.id})
+        if (albums.length > 0) {
+            return res.status(422).send({message: 'Delete related fields first'});
+        }
+        await Artist.findOneAndRemove({_id: req.params.id});
+        return res.send({ message: `${req.params.id} removed` });
+    } catch (e) {
+        return res.status(422).send(e);
     }
 });
 
--- models/Artist.js
+++ models/Artist.js
@@ -8,7 +8,13 @@ const ArtistSchema = new Schema({
         required: true
     },
     image: String,
-    description: String
+    description: String,
+    published: {
+        type: Boolean,
+        required: true,
+        default: "false",
+        enum: ["false", "true"]
+    }
 });
 
 const Artist = mongoose.model("Artist", ArtistSchema);
