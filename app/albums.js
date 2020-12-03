d038cc9e15d7503385eaec6fbe8b1f0ea51973b0
maksimus <gorlovmaks23@gmail.com>
2020-12-03 17:19:07 +0600

    HW#91 Обновила файл albums.js

--- app/albums.js
+++ app/albums.js
@@ -4,9 +4,6 @@ const path = require("path");
 const {nanoid} = require("nanoid");
 const config = require("../config");
 const Album = require("../models/Album");
-const Track = require("../models/Track");
-const auth = require("../middleware/auth");
-const permit = require("../middleware/permit");
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -62,30 +59,6 @@ router.post("/", upload.single("image"), async (req, res) => {
         res.send(album);
     } catch (e) {
         res.status(400).send(e);
-    }
-});
-
-router.put("/:id", [auth, permit('admin')], async (req, res) => {
-    try {
-        const album = await Album.findById(req.params.id);
-        album.published = !album.published
-        await album.save();
-        return res.send({ message: `${req.params.id} published` });
-    } catch (e) {
-        return res.status(422).send(e);
-    }
-});
-
-router.delete("/:id", [auth, permit('admin')], async (req, res) => {
-    try {
-        const tracks = await Track.find({album: req.params.id})
-        if (tracks.length > 0) {
-            return res.status(422).send({message: 'Delete related fields first'});
-        }
-        await Album.findOneAndRemove({_id: req.params.id});
-        return res.send({ message: `${req.params.id} removed` });
-    } catch (e) {
-        return res.status(422).send(e);
     }
 });
 
