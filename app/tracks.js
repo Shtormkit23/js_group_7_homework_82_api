5be9e3d62410e6255b6ed4f3092e6eacf6e1c347
maksimus <gorlovmaks23@gmail.com>
2020-12-03 17:35:01 +0600

    HW#91 Обновила роуты

--- app/tracks.js
+++ app/tracks.js
@@ -1,8 +1,6 @@
 const router = require("express").Router();
 const Track = require("../models/Track");
 const Album = require("../models/Album");
-const auth = require("../middleware/auth");
-const permit = require("../middleware/permit");
 
 router.get('/', async (req, res) => {
     try {
@@ -33,26 +31,6 @@ router.post("/", async (req, res) => {
         res.send(track);
     } catch (e) {
         res.status(400).send(e);
-    }
-});
-
-router.put("/:id", [auth, permit('admin')], async (req, res) => {
-    try {
-        const track = await Track.findById(req.params.id);
-        track.published = !track.published
-        await track.save();
-        return res.send({ message: `${req.params.id} published` });
-    } catch (e) {
-        return res.status(422).send(e);
-    }
-});
-
-router.delete("/:id", [auth, permit('admin')], async (req, res) => {
-    try {
-        await Track.findOneAndRemove({_id: req.params.id});
-        return res.send({ message: `${req.params.id} removed` });
-    } catch (e) {
-        return res.status(422).send(e);
     }
 });
 
