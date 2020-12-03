734cf75f3b170c123aeac137c5499bab00490cf2
maksimus <gorlovmaks23@gmail.com>
2020-12-03 17:34:39 +0600

    HW#91 Добавила роуты для публикации сущностей

--- app/publish.js
+++ app/publish.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 const Album = require("../models/Album");
 const Artist = require("../models/Artist");
 const Track = require("../models/Track");
+const auth = require("../middleware/auth");
+const permit = require("../middleware/permit");
 
 router.get('/albums', async (req, res) => {
     try {
@@ -32,7 +34,37 @@ router.get('/tracks', async (req, res) => {
     }
 });
 
+router.put("/tracks/:id", [auth, permit('admin')], async (req, res) => {
+    try {
+        const track = await Track.findById(req.params.id);
+        track.published = !track.published
+        await track.save();
+        return res.send({ message: `${req.params.id} published` });
+    } catch (e) {
+        return res.status(422).send(e);
+    }
+});
 
+router.put("/artists/:id", [auth, permit('admin')], async (req, res) => {
+    try {
+        const artist = await Artist.findById(req.params.id);
+        artist.published = !artist.published
+        await artist.save();
+        return res.send({ message: `${req.params.id} published` });
+    } catch (e) {
+        return res.status(422).send(e);
+    }
+});
 
+router.put("/albums/:id", [auth, permit('admin')], async (req, res) => {
+    try {
+        const album = await Album.findById(req.params.id);
+        album.published = !album.published
+        await album.save();
+        return res.send({ message: `${req.params.id} published` });
+    } catch (e) {
+        return res.status(422).send(e);
+    }
+});
 
 module.exports = router;
